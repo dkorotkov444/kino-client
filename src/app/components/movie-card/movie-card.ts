38a1b7c413a3c6832c08d44f918d40e6c7706c8f
DK-CF <korotkovadvisory@gmail.com>
2025-12-13 17:30:18 +0100

    A6-E6.4 Created movie-card and welcome-page components.

---
+++ src/app/components/movie-card/movie-card.ts
@@ -0,0 +1,33 @@
+// src/app/movie-card/movie-card.ts
+
+import { Component, Injectable, inject, OnInit } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
+import { Observable } from 'rxjs';
+import { MovieService } from '../../services/movie.service';
+
+@Component({
+    standalone: true,
+    selector: 'kino-movie-card',
+    imports: [
+        AsyncPipe,
+        MatCardModule, 
+        MatDialogModule,
+    ],
+    templateUrl: './movie-card.html',
+    styleUrl: './movie-card.scss',
+})
+export class MovieCardComponent implements OnInit {
+    private movieService = inject(MovieService);
+    // The movies array is an Observable that emits the array. Observable<any> matches the service's current return type.
+    public movies$!: Observable<any>; // Note: Use a specific type instead of 'any' later!
+
+    constructor() {}
+
+    ngOnInit(): void {
+        // Directly assign the Observable returned by the service.
+        this.movies$ = this.movieService.getAllMovies();
+    }
+
+}
---
+++ src/app/components/welcome-page/welcome-page.ts
@@ -0,0 +1,41 @@
+import { Component, inject, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { UserRegistrationFormComponent } from '../user-registration-form/user-registration-form';
+import { UserLoginFormComponent } from '../user-login-form/user-login-form';
+
+@Component({
+  selector: 'kino-welcome-page',
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    MatDialogModule,
+    MatButtonModule,
+  ],
+  templateUrl: './welcome-page.html',
+  styleUrl: './welcome-page.scss',
+})
+export class WelcomePageComponent {
+    protected readonly title = signal('KINO App');
+
+    readonly dialog: MatDialog = inject(MatDialog);
+    /**
+     * Opens the user registration form inside an Angular Material Dialog.
+     */
+    openUserRegistrationDialog(): void {
+        this.dialog.open(UserRegistrationFormComponent, {
+        width: '280px',
+        });
+    }
+
+    /**
+     * Opens the user login form inside an Angular Material Dialog.
+     */
+    openUserLoginDialog(): void {
+        this.dialog.open(UserLoginFormComponent, {
+            width: '280px',
+        });
+    }
+}
